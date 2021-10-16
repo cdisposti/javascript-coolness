59edf975c008237657723f34c73316a88c9c56b0
Courtney Disposti <2ndmousemedia@gmail.com>
2021-10-16 16:11:11 -0400

    new function for how old you were in what year

---
+++ howOld.js
@@ -0,0 +1,23 @@
+// Write your function here:
+const howOld = (age, year) => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  let calcAge = 0;
+  let calcYear = 0;
+  let yearBorn = currentYear - age;
+  if (year > currentYear) {
+    calcAge = age + (year - currentYear)
+  return `You will be ${calcAge} in the year ${year}`
+  } else if (year < yearBorn) {
+    calcYear = yearBorn - year
+  return  `The year ${year} was ${calcYear} years before you were born`
+  } else if ( year < currentYear && year > yearBorn) {
+  calcAge = (year - yearBorn)
+  return `You were ${calcAge} in the year ${year}`
+}
+}
+
+
+
+// Once your function is written, write function calls to test your code!
+console.log(howOld(44, 1970));
